a34e7fa6ee98a142e36e7c00e51a09e813d21735
pjm6857 <pjm6857@gmail.com>
2026-02-10 09:00:43 +0900

    fix: ai 서버 설정 삭제

--- src/main/java/dev/gmpark/cors/CorsApplication.java
+++ src/main/java/dev/gmpark/cors/CorsApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CorsApplication {
 
-    public static int AI_SERVER_PORT = 8000; // 기본값
 
     public static void main(String[] args) {
         SpringApplication.run(CorsApplication.class, args);
